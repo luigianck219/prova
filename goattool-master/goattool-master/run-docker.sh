02888a2bf30a4156794902430549d9b1fd90a0df
luigianck219 <luigianck219@gmail.com>
2026-03-20 23:54:00 +0100

    Add files via upload

---
+++ goattool-master/goattool-master/run-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# I'm not that familiar with docker. Send a PR if something is wrong (^.^)
+
+BASE_DIR=$(realpath "$(dirname "$BASH_SOURCE")")
+if [[ ! -d "$BASE_DIR/auth" ]]; then
+    echo "Creating Auth Directory.."
+	mkdir -p "$BASE_DIR/auth"
+fi
+
+CONTAINER="goattool"
+IMAGE="htrtech/goattool:latest"
+IMG_MIRROR="ghcr.io/htr-tech/goattool:latest"
+MOUNT_LOCATION=${BASE_DIR}/auth
+check_container=$(docker ps --all --format "{{.Names}}")
+
+if [[ ! $check_container == $CONTAINER ]]; then
+	echo "Creating new container..."
+	docker create \
+		--interactive --tty \
+		--volume ${MOUNT_LOCATION}:/goattool/auth/ \
+		--network host \
+		--name "${CONTAINER}" \
+		"${IMAGE}"
+fi
+
+docker start --interactive "${CONTAINER}"
+
